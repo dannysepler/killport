89d4c6ef94084c6101516f7e031b83730dd1fa22
Danny Sepler <dannysepler@gmail.com>
2022-06-04 12:08:42 -0400

    use proc.kill

--- tests/test_killport.py
+++ tests/test_killport.py
@@ -2,26 +2,17 @@ from __future__ import annotations
 
 import http.server
 import re
-import signal
 
 import pytest
 
-from killport import TERM_SIGNAL, get_processes, kill_ports
+from killport import get_processes, kill_ports
 
 PORT = 2345
 
 
-@pytest.fixture(scope='session', autouse=True)
-def term_handler():
-    # if a TERM_SIGNAL (such as SIGTERM) is given, pytest abruptly stops
-    # https://github.com/pytest-dev/pytest/issues/5243
-
-    def do_nothing(*args, **kwargs):
-        ...
-
-    orig = signal.signal(TERM_SIGNAL, do_nothing)
-    yield
-    signal.signal(TERM_SIGNAL, orig)
+@pytest.fixture(autouse=True)
+def mock_kill(mocker):
+    return mocker.patch('psutil.os.kill')
 
 
 @pytest.fixture
@@ -41,7 +32,7 @@ def test_get_processes_when_process(server):
     assert len(get_processes([PORT])) == 1
 
 
-def test_kills_port_when_desired(server, capsys):
+def test_kills_port_when_desired(server, capsys, mock_kill):
     kill_ports(ports=[PORT])
 
     output = capsys.readouterr().out
@@ -50,9 +41,10 @@ def test_kills_port_when_desired(server, capsys):
         rf'- \S* \(pid \d+\) on port {PORT}',
         output,
     )
+    assert mock_kill.call_count == 1
 
 
-def test_kills_processes_in_order_of_port(capsys):
+def test_kills_processes_in_order_of_port(capsys, mock_kill):
     Handler = http.server.BaseHTTPRequestHandler
 
     # Spins off 5555 then 1111, we want to kill 1111 first
@@ -68,19 +60,21 @@ def test_kills_processes_in_order_of_port(capsys):
         r'- \S* \(pid \d+\) on port 5555',
         output,
     )
+    assert mock_kill.call_count == 2
 
     httpd_5555.server_close()
     httpd_1111.server_close()
 
 
-def test_doesnt_destroy_irrelevant_port(server, capsys):
+def test_doesnt_destroy_irrelevant_port(server, capsys, mock_kill):
     kill_ports(ports=[PORT + 1])
 
     output = capsys.readouterr().out
     assert output == ''
+    assert mock_kill.call_count == 0
 
 
-def test_view_only_doesnt_destroy_port(server, capsys):
+def test_view_only_doesnt_destroy_port(server, capsys, mock_kill):
     kill_ports(ports=[PORT], view_only=True)
 
     output = capsys.readouterr().out
@@ -89,3 +83,4 @@ def test_view_only_doesnt_destroy_port(server, capsys):
         rf'- \S* \(pid \d+\) on port {PORT}',
         output,
     )
+    assert mock_kill.call_count == 0
